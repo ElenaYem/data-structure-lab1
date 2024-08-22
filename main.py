f376d941361530de88b83abc36166eccce5cb2ed
Elena May Olaivar <torreselenamay@gmail.com>
2024-08-22 11:52:36 +0800

    One

---
+++ main.py
@@ -0,0 +1,16 @@
+
+numberOne =input("Enter first number: ")
+numberTwo =input("Enter second number: ")
+
+if numberOne < numberTwo:
+    print(numberOne + " is  less than " + numberTwo)
+elif numberTwo > numberOne:
+    print(numberTwo + " is greater than " + numberOne)
+elif numberOne == numberOne:
+    print(numberOne + " is equals to " + numberOne)
+
+# cm1 = input("Enter classmate name 1: ")
+# cm2 = input("Enter classmate name 2: ")
+# cm3 = input("Enter classmate name 3: ")
+#
+# print(f"Your classmate are: {cm1}, {cm2} & {cm3}")
